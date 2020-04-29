f0f1676d535aac8359f366d23c36eb2d34099b29
unknown <andrey.maksuta1714@gmail.com>
2020-04-29 10:18:55 +0300

    run2

---
+++ module-1/task-1.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const GOODS_NAME = "Генератор защитного поля";
+let patternStringMessage
+let goodsPrice;
+goodsPrice = 1000;
+
+
+patternStringMessage = `Выбран ${GOODS_NAME}, цена за штуку ${goodsPrice} кредитов`;
+console.log(patternStringMessage);
+
+goodsPrice = 2000;
+patternStringMessage = `Выбран ${GOODS_NAME}, цена за штуку ${goodsPrice} кредитов`;
+console.log(patternStringMessage);
