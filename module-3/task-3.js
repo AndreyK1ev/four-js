6a8b88fa28959a620c3ac04eb45470d871ee83e7
unknown <andrey.maksuta1714@gmail.com>
2020-05-03 17:07:57 +0300

    task-3

---
+++ module-3/task-3.js
@@ -0,0 +1,43 @@
+'use strict';
+
+function findBestEmployee(employees) {
+  let highestEmployeeValue = 0;
+  let highestEmployeeName;
+  const EMPLOYEES = Object.entries(employees);
+
+  for (const i = 0; i < EMPLOYEES.length; i++) {
+    for (const key in employees) {
+      if (highestEmployeeValue < employees[key]) {
+        highestEmployeeValue = employees[key];
+        highestEmployeeName = key;
+      }
+    }
+    return highestEmployeeName;
+  }
+}
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
